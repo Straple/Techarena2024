c18efd85684235fa2fa3bce1ccddfcd25a10e066
Straple <hadayegor@gmail.com>
2024-04-29 14:43:15 +0300

    commit tester

--- main.cpp
+++ main.cpp
@@ -1,239 +1,69 @@
 #include "bits/stdc++.h"
 
-namespace KopeliovichStream {
+using namespace std;
 
-/**
- * Author: Sergey Kopeliovich (Burunduk30@gmail.com)
- */
+struct UserInfo {
+    int rbNeed;
+    int beam;
+    int id;
+};
 
-#define VERSION "0.1.8"
+struct Interval {
+    int start, end;
+    std::vector<int> users;
+};
 
-#include <algorithm>
-#include <cassert>
-#include <cstdio>
+vector<Interval> Solver(int N, int M, int K, int J, int L,
+                        vector<Interval> reservedRBs,
+                        vector<UserInfo> userInfos
+) {
 
-/** Fast allocation */
+}
 
-#ifdef FAST_ALLOCATOR_MEMORY
-    int allocator_pos = 0;
-    char allocator_memory[(int)FAST_ALLOCATOR_MEMORY];
+struct TestData {
+    int N, M, K, J, L;
+    vector<Interval> reservedRBs;
+    vector<UserInfo> userInfos;
+};
 
-    inline void *operator new(size_t n) {
-        // fprintf(stderr, "n=%ld\n", n);
-        char *res = allocator_memory + allocator_pos;
-        assert(n <= (size_t)((int)FAST_ALLOCATOR_MEMORY - allocator_pos));
-        allocator_pos += n;
-        return (void *)res;
+TestData read_test(istream &input) {
+    int N, M, K, J, L;
+    input >> N >> M >> K >> J >> L;
+
+    vector<Interval> reservedRBs(K);
+    for (auto &[start, end, users]: reservedRBs) {
+        input >> start >> end;
     }
 
-    inline void operator delete(void *) noexcept {
+    vector<UserInfo> userInfos(N);
+    for (int u = 0; u < N; u++) {
+        userInfos[u].id = u;
+        input >> userInfos[u].rbNeed >> userInfos[u].beam;
     }
 
-    inline void operator delete(void *, size_t) noexcept {
-    }
+    return TestData{N, M, K, J, L, reservedRBs, userInfos};
+}
 
-    // inline void * operator new [] (size_t) { assert(0); }
-    // inline void operator delete [] (void *) { assert(0); }
-#endif
+double get_solution_score(const TestData &testdata) {
+    auto result = Solver(testdata.N, testdata.M, testdata.K, testdata.J, testdata.L, testdata.reservedRBs,
+                         testdata.userInfos);
 
-/** Fast input-output */
-
-    template<class T = int>
-    inline T readInt();
-
-    inline double readDouble();
-
-    inline int readUInt();
-
-    inline int readChar();  // first non-blank character
-    inline void readWord(char *s);
-
-    inline bool readLine(char *s);  // do not save '\n'
-    inline bool isEof();
-
-    inline int getChar();
-
-    inline int peekChar();
-
-    inline bool seekEof();
-
-    inline void skipBlanks();
-
-    template<class T>
-    inline void writeInt(T x, char end = 0, int len = -1);
-
-    inline void writeChar(int x);
-
-    inline void writeWord(const char *s);
-
-    inline void
-    writeDouble(double x, int len = 16);  // works correct only for |x| < 2^{63}
-    inline void flush();
-
-    static struct buffer_flusher_t {
-        ~buffer_flusher_t() {
-            flush();
-        }
-    } buffer_flusher;
-
-/** Read */
-
-    static const int buf_size = 4096 * 4;
-
-    static unsigned char buf[buf_size];
-    static int buf_len = 0, buf_pos = 0;
-
-    inline bool isEof() {
-        if (buf_pos == buf_len) {
-            buf_pos = 0, buf_len = fread(buf, 1, buf_size, stdin);
-            if (buf_pos == buf_len)
-                return 1;
-        }
-        return 0;
-    }
-
-    inline int getChar() {
-        return isEof() ? -1 : buf[buf_pos++];
-    }
-
-    inline int peekChar() {
-        return isEof() ? -1 : buf[buf_pos];
-    }
-
-    inline bool seekEof() {
-        int c;
-        while ((c = peekChar()) != -1 && c <= 32)
-            buf_pos++;
-        return c == -1;
-    }
-
-    inline void skipBlanks() {
-        while (!isEof() && buf[buf_pos] <= 32U)
-            buf_pos++;
-    }
-
-    inline int readChar() {
-        int c = getChar();
-        while (c != -1 && c <= 32)
-            c = getChar();
-        return c;
-    }
-
-    inline int readUInt() {
-        int c = readChar(), x = 0;
-        while ('0' <= c && c <= '9')
-            x = x * 10 + (c - '0'), c = getChar();
-        return x;
-    }
-
-    template<class T>
-    inline T readInt() {
-        int minus = 0, c = readChar();
-        T x = 0;
-        if (c == '-')
-            minus = 1, c = getChar();
-        else if (c == '+')
-            c = getChar();
-        for (; '0' <= c && c <= '9'; c = getChar())
-            if (minus)
-                x = x * 10 - (c - '0');  // take care about -2^{31}
-            else
-                x = x * 10 + (c - '0');
-        return x;
-    }
-
-    inline double readDouble() {
-        int s = 1, c = readChar();
-        double x = 0;
-        if (c == '-')
-            s = -1, c = getChar();
-        while ('0' <= c && c <= '9')
-            x = x * 10 + (c - '0'), c = getChar();
-        if (c == '.') {
-            c = getChar();
-            double coef = 1;
-            while ('0' <= c && c <= '9')
-                x += (c - '0') * (coef *= 1e-1), c = getChar();
-        }
-        return s == 1 ? x : -x;
-    }
-
-    inline void readWord(char *s) {
-        int c = readChar();
-        while (c > 32)
-            *s++ = c, c = getChar();
-        *s = 0;
-    }
-
-    inline bool readLine(char *s) {
-        int c = getChar();
-        while (c != '\n' && c != -1)
-            *s++ = c, c = getChar();
-        *s = 0;
-        return c != -1;
-    }
-
-/** Write */
-
-    static int write_buf_pos = 0;
-    static char write_buf[buf_size];
-
-    inline void writeChar(int x) {
-        if (write_buf_pos == buf_size)
-            fwrite(write_buf, 1, buf_size, stdout), write_buf_pos = 0;
-        write_buf[write_buf_pos++] = x;
-    }
-
-    inline void flush() {
-        if (write_buf_pos) {
-            fwrite(write_buf, 1, write_buf_pos, stdout), write_buf_pos = 0;
-            fflush(stdout);
+    vector<int> user_score(testdata.N);
+    for (auto [start, end, users]: result) {
+        for (int id: users) {
+            user_score[id] += end - start;
         }
     }
 
-    template<class T>
-    inline void writeInt(T x, char end, int output_len) {
-        if (x < 0)
-            writeChar('-');
-
-        char s[24];
-        int n = 0;
-        while (x || !n)
-            s[n++] = '0' + abs((int) (x % 10)),
-                    x /= 10;  // `abs`: take care about -2^{31}
-        while (n < output_len)
-            s[n++] = '0';
-        while (n--)
-            writeChar(s[n]);
-        if (end)
-            writeChar(end);
+    int sum_score = 0;
+    for (int u = 0; u < testdata.N; u++) {
+        sum_score += std::min(user_score[u], testdata.userInfos[u].rbNeed);
     }
-
-    inline void writeWord(const char *s) {
-        while (*s)
-            writeChar(*s++);
-    }
-
-    inline void writeDouble(double x, int output_len) {
-        if (x < 0)
-            writeChar('-'), x = -x;
-        assert(x <= (1LLU << 63) - 1);
-        long long t = (long long) x;
-        writeInt(t), x -= t;
-        writeChar('.');
-        for (int i = output_len - 1; i > 0; i--) {
-            x *= 10;
-            t = std::min(9, (int) x);
-            writeChar('0' + t), x -= t;
-        }
-        x *= 10;
-        t = std::min(9, (int) (x + 0.5));
-        writeChar('0' + t);
-    }
-
-}  // namespace KopeliovichStream
+    return sum_score;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
     return 0;
 }
