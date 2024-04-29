49b98fb73bfa336469522be87d21ffc4243a59b2
Straple <hadayegor@gmail.com>
2024-04-29 15:10:23 +0300

    add assert add validate answer intervals

--- main.cpp
+++ main.cpp
@@ -1,3 +1,32 @@
+#include <iostream>
+
+///////// !!!
+#define MY_DEBUG_MODE
+///////// !!!
+
+#ifdef MY_DEBUG_MODE
+
+#define FAILED_ASSERT(message)                                                \
+    {                                                                         \
+        std::cerr << "assert failed at " __FILE__ << ":" << __LINE__ << '\n'; \
+        std::cerr << "message: \"" << (message) << "\"\n";                    \
+        std::exit(0);                                                         \
+    }
+
+#define ASSERT(condition, message) \
+    if (!(condition))              \
+    FAILED_ASSERT(message)
+
+//_STL_VERIFY(condition, message) // don't work on CLion
+
+#else
+
+#define ASSERT(condition, message) condition /**THIS CONDITION VERY IMPORTANT */
+
+#endif// MY_DEBUG_MODE
+
+#pragma once
+
 #include "bits/stdc++.h"
 
 using namespace std;
@@ -48,16 +77,43 @@ double get_solution_score(const TestData &testdata) {
     auto result = Solver(testdata.N, testdata.M, testdata.K, testdata.J, testdata.L, testdata.reservedRBs,
                          testdata.userInfos);
 
+    for (int i = 0; i < result.size(); i++) {
+        for (int j = i + 1; j < result.size(); j++) {
+            ASSERT(result[i].end <= result[j].start || result[j].end <= result[i].start,
+                   "answer interval is invalid: intersect");
+        }
+    }
+
     vector<int> user_score(testdata.N);
+    vector<int> user_min(testdata.N, 1e9);
+    vector<int> user_max(testdata.N, -1e9);
     for (auto [start, end, users]: result) {
+        // validate interval
+        {
+            ASSERT(users.size() <= testdata.L, "answer interval is invalid: users more than L");
+            ASSERT(start < end, "answer interval is invalid: start >= end");
+            for (auto [reserved_start, reserved_end, reserved_users]: testdata.reservedRBs) {
+                ASSERT(end <= reserved_start || reserved_end <= start, "answer interval intersect with reservedRbs");
+            }
+        }
+        set<int> unique_beams;
         for (int id: users) {
             user_score[id] += end - start;
+
+            user_min[id] = min(user_min[id], start);
+            user_max[id] = max(user_max[id], end);
+            unique_beams.insert(testdata.userInfos[id].beam);
         }
+
+        ASSERT(unique_beams.size() == users.size(), "answer interval is invalid: have equal user beams");
     }
 
     int sum_score = 0;
     for (int u = 0; u < testdata.N; u++) {
         sum_score += std::min(user_score[u], testdata.userInfos[u].rbNeed);
+
+        ASSERT(user_score[u] == user_max[u] - user_min[u],
+               "answer interval is invalid: user have no continuous interval");
     }
     return sum_score;
 }
