490db6b994d25a723c43155697ca11772f683258
Artem Brezhnev <brezhart@gmail.com>
2024-04-29 13:48:51 +0300

    test commit

---
+++ cmake-build-debug/CMakeFiles/Techarena2024.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Techarena2024.dir/main.cpp.o"
+  "CMakeFiles/Techarena2024.dir/main.cpp.o.d"
+  "Techarena2024"
+  "Techarena2024.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Techarena2024.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
